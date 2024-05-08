2eaebdb5bb4c07266653bf87f42cd64985cbe075
kimdongjun <r2222911r@ajou.ac.kr>
2024-05-08 13:19:13 +0900

    :sparkles: feat: Entity 생성
    Entity 생성

---
+++ src/main/java/sogong/solveconomy/domain/Dict.java
@@ -0,0 +1,27 @@
+package sogong.solveconomy.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import sogong.solveconomy.domain.common.BaseEntity;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Dict extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String keyword;
+
+    @Column(nullable = false)
+    private String definition;
+
+}

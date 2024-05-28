71e94885e7a37c327a80df5e84b6140f36d8abcb
kimdongjun <r2222911r@ajou.ac.kr>
2024-05-29 01:00:46 +0900

    :recycle: refactor: DictRepository 메서드 추가
    DictRepository 메서드 추가

--- src/main/java/sogong/solveconomy/repository/DictRepository.java
+++ src/main/java/sogong/solveconomy/repository/DictRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import sogong.solveconomy.domain.Dict;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface DictRepository extends JpaRepository<Dict, Long> {
     List<Dict> findByKeywordContaining(String keyword);
+
+    Optional<Dict> findByKeyword(String keyword);
 }

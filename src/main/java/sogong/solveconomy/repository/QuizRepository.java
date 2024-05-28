95add8a2e70e721755fe0b60e534e1d8ca6e0efc
kimdongjun <r2222911r@ajou.ac.kr>
2024-05-29 01:05:18 +0900

    :sparkles: feat: QuizRepository 생성
    QuizRepository 생성

---
+++ src/main/java/sogong/solveconomy/repository/QuizRepository.java
@@ -0,0 +1,9 @@
+package sogong.solveconomy.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import sogong.solveconomy.domain.Quiz;
+
+import java.util.Optional;
+
+public interface QuizRepository extends JpaRepository<Quiz, Long> {
+}

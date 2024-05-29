7b8b40495c52edacb9123c6af24ee3e11fc0b2a4
kimdongjun <r2222911r@ajou.ac.kr>
2024-05-29 11:09:50 +0900

    :recycle: refactor: main 함수 변경
    main 함수 변경

--- src/main/java/sogong/solveconomy/SolveconomyApplication.java
+++ src/main/java/sogong/solveconomy/SolveconomyApplication.java
@@ -2,8 +2,10 @@ package sogong.solveconomy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SolveconomyApplication {
 
 	public static void main(String[] args) {
